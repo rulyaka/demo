69b2ba32185b691259cd527a083e22a5e2119e86
CybertekSchool <github@cybertekschool.com>
2019-08-04 17:09:32 -0400

    initial commit

---
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
@@ -0,0 +1,31 @@
+package com.vytrack.step_definitions;
+
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class LoginStepDefs {
+    @Given("I am on the login page")
+    public void i_am_on_the_login_page() {
+        System.out.println("Opening the login page");
+    }
+
+
+    @When("I login as a driver")
+    public void i_login_as_a_driver() {
+        System.out.println("Logging in as a driver");
+    }
+
+    @Then("I should be able to see the dashboard page")
+    public void i_should_be_able_to_see_the_dashboard_page() {
+        System.out.println("Verifying dashboard page");
+    }
+
+    @When("I login as a sales manager")
+    public void i_login_as_a_sales_manager() {
+        System.out.println("Logging in as a sales manager");
+    }
+
+
+}
---
+++ src/test/resources/com/vytrack/features/Login.feature
@@ -0,0 +1,15 @@
+Feature: Login
+  Agile story:
+    As a user,
+    when I enter valid login credentials
+    I should be able to see the dashboard page
+
+  Scenario: Login as driver
+    Given I am on the login page
+    When I login as a driver
+    Then I should be able to see the dashboard page
+
+  Scenario: Login as a sales manager
+    Given I am on the login page
+    When I login as a sales manager
+    Then I should be able to see the dashboard page
