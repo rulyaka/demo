a5cafa1bec3075b3e6d721a8673aee7950f43b31
CybertekSchool <github@cybertekschool.com>
2019-08-07 19:53:36 -0400

    implemented the driver login scenario

--- src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
@@ -1,19 +1,29 @@
 package com.vytrack.step_definitions;
 
+import com.vytrack.pages.LoginPage;
+import com.vytrack.utilities.BrowserUtils;
+import com.vytrack.utilities.ConfigurationReader;
+import com.vytrack.utilities.Driver;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 
 public class LoginStepDefs {
 
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
+
         System.out.println("Opening the login page");
+        Driver.get().get(ConfigurationReader.get("url"));
     }
 
     @Then("I should be able to see the dashboard page")
     public void i_should_be_able_to_see_the_dashboard_page() {
         System.out.println("Verifying dashboard page");
+        BrowserUtils.waitFor(2);
+        Assert.assertTrue(Driver.get().getTitle().contains("Dashboard"));
+
     }
 
     @When("I login as a sales manager")
@@ -21,5 +31,15 @@ public class LoginStepDefs {
         System.out.println("Logging in as a sales manager");
     }
 
+    @When("I login as a driver")
+    public void i_login_as_a_driver() {
+        System.out.println("Logging in as a driver");
+        String username = ConfigurationReader.get("driver_username");
+        String password = ConfigurationReader.get("driver_password");
+
+        LoginPage loginPage = new LoginPage();
+        loginPage.login(username, password);
+
+    }
 
 }
