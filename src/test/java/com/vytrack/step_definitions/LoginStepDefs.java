202c2940aedf47a8fdaed7dc7d10ae69723989d6
CybertekSchool <github@cybertekschool.com>
2019-08-07 19:30:29 -0400

    removed pages reference

--- src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
+++ src/test/java/com/vytrack/step_definitions/LoginStepDefs.java
@@ -7,16 +7,11 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class LoginStepDefs {
-    private Pages pages;
 
-    public LoginStepDefs(Pages pages) {
-        this.pages = pages;
-    }
 
     @Given("I am on the login page")
     public void i_am_on_the_login_page() {
-        // Write code here that turns the phrase above into concrete actions
-        throw new cucumber.api.PendingException();
+        System.out.println("Opening the login page");
     }
 
     @Then("I should be able to see the dashboard page")
